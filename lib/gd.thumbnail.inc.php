0b3e665f4615e0897395b5a2cc4f03a5085927c6
Tommy <designer@designer-VirtualBox.(none)>
2011-11-24 14:57:27 +0000

    Download s3 image befor creating thumb

--- lib/gd.thumbnail.inc.php
+++ lib/gd.thumbnail.inc.php
@@ -113,17 +113,13 @@ class ngg_Thumbnail {
      *
      */
     function ngg_Thumbnail($fileName,$no_ErrorImage = false) {
-        //make sure the GD library is installed
-    	error_log('ngg_thumb start');
 
         if(!function_exists("gd_info")) {
         	echo 'You do not have the GD Library installed.  This class requires the GD library to function properly.' . "\n";
         	echo 'visit http://us2.php.net/manual/en/ref.image.php for more information';
-        	error_log('no gd');
             exit;
         }
 
-        error_log('if passed');
     	//initialize variables
         $this->errmsg               = '';
         $this->error                = false;
